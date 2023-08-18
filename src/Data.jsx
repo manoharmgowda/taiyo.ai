84d3b53f6f7b77230d0031793c9a69826872b2c0
manoharmgowda <49287361+manoharmgowda@users.noreply.github.com>
2023-08-18 22:33:09 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    firstName: "manohar",
+    lastName: "m",
+    id: 1,
+  },
+  {
+    firstName: "the",
+    lastName: "groot",
+    id: 2,
+  },
+  {
+    firstName: "anand",
+    lastName: "jayaram",
+    id: 3,
+  },
+];
